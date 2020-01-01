6646ccdb6b1f883c4de5c280b85a38cb35ab19ae
luckydan61 <luckydan_61@qq.com>
2020-01-01 08:40:43 +0800

    Move HelloWorld into Folder

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("This is prepare for Algorithm 4 practise.\n");
+    return 0;
+}
---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is prepare for Algorithm 4 practise.");
+    }
+}
